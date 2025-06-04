d7ad265027129c67d463b46facebcce5a67c064b
zypherson <taylorzypherson@gmail.com>
2025-06-04 14:33:09 -0400

    Added widget

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Dashboard ready");
   });
+
+function createWidget(title, content) {
+  const widget = document.createElement('div');
+  widget.className = 'widget';
   
+
+  const widgetTitle = document.createElement('h2');
+  widgetTitle.textContent = title
+
+  const widgetContent = document.createElement('p');
+  widgetContent.innerHTML = content;
+
+  widget.appendChild(widgetTitle);
+  widget.appendChild(widgetContent);
+}
