820dc1a6f9d5000a399d7681cf7d9141eb42b124
zypherson <taylorzypherson@gmail.com>
2025-06-06 12:35:58 -0400

    added new widget

--- script.js
+++ script.js
@@ -15,4 +15,20 @@ function createWidget(title, content) {
 
   widget.appendChild(widgetTitle);
   widget.appendChild(widgetContent);
+  return widget
 }
+
+const quotes = [
+    "Believe in youself",
+    "Be great.",
+    "Every day is a new day."
+  ];
+
+  function displayQuote() {
+    const day = new Date().getDate();
+    const index = day % quotes.length;
+    document.getElementById('quote').textContent = quotes[index];
+  }
+  
+  displayQuote();
+  
